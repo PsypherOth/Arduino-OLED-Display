8269590f555a4b22c0c8be35b9dc30f429675111
PsypherOth <sam.mccrowell@gmail.com>
2025-02-08 16:10:23 -0500

    Push #1
    Setup with screen definition and initializing.

--- IDP_OLEDDisplay/IDP_OLEDDisplay.ino
+++ IDP_OLEDDisplay/IDP_OLEDDisplay.ino
@@ -1,6 +1,18 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET_PIN -1
+#define SCREEN_ADDRESS 0x3C
+
+Adafruit_SSD1306 screen(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET_PIN);
 void setup() {
   // put your setup code here, to run once:
+  screen.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS);
 
+  screen.clearDisplay();
 }
 
 void loop() {
